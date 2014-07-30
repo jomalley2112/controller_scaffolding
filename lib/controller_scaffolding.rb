c7325ed0cb64a6e2b5c3fa9dcc7833ce1041c53b
John O'Malley <jom@nycap.rr.com>
2014-07-29 23:18:44 -0400

    Require SnS gem in controller_scaffolding.rb.

--- lib/controller_scaffolding.rb
+++ lib/controller_scaffolding.rb
@@ -5,6 +5,5 @@ module ControllerScaffolding
 	require "factory_girl_rails" if Rails.env == 'test'
 	require 'controller_scaffolding/add_generator' if defined?(Rails)
 	require 'controller_scaffolding/railtie' if defined?(Rails)
-	require "sql_search_n_sort/sql_sort_setup"
-	require 'sql_search_n_sort/sql_searchable_sortable'
+	require 'sql_search_n_sort' #The actual gem instead of all the separate files
 end
--- lib/controller_scaffolding/version.rb
+++ lib/controller_scaffolding/version.rb
@@ -1,3 +1,3 @@
 module ControllerScaffolding
-  VERSION = "1.0.1"
+  VERSION = "1.0.2"
 end
